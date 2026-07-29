72746836771ef3df3c7b90744b8bae32186f28f3
Jesús Hernández <yisus.tarnished@gmail.com>
2026-07-29 12:14:34 -0600

    Se conectan páginas de Nosotros y Contactos, con inyección por JS, igual se agrega menú responsive para dispositivos móviles

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,19 @@
-script
+import { iniciarNavbar } from "../JS/navbar.js";
+
+
+async function cargarNavbar() {
+
+
+    const respuesta = await fetch("../HTML/navbar.html");
+    const navbar = await respuesta.text();
+
+
+    document.getElementById("navbarInject").innerHTML = navbar;
+
+
+    iniciarNavbar();
+}
+
+
+cargarNavbar();
+//script
