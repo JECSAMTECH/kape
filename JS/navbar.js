a0b63e98f6f280ef28f6bb3224f33e1d089d7d05
Steven León <104881306+Superfier@users.noreply.github.com>
2026-08-24 23:53:22 -0600

    Revisiones (#115)
    * Se icluyó iconos a los inputs
    
    * Se agrego icono clicleable mostrar contraseña-ocultar Se añadió la logica a ese icono
    
    * Se añadió la logica a ese icono
    
    * Se reparó el index
    
    * Se reviso la ruta a Index
    
    * Se realizaron ajustes al navbar se modificaron algunos archivos,  CSS/inicio.css, modified:CSS/navbar.css modified:HTML/navbar.html, modified:JS/navbar.js, index.html
    
    ---------
    
    Co-authored-by: Jesús Hernández Delgado <yisus.tarnished@gmail.com>

--- JS/navbar.js
+++ JS/navbar.js
@@ -5,6 +5,20 @@ export function iniciarNavbar() {
     modalNavbar.addEventListener('shown.bs.modal', () => {
         inputNavbarSearch.focus();
     });
+
+    // Marca el link activo según la URL actual
+    const currentPath = window.location.pathname.replace(/\/$/, "").split("/").pop() || "index.html";
+
+    document.querySelectorAll(".navbar-nav .nav-item").forEach(item => {
+        item.classList.remove("active");
+    });
+
+    document.querySelectorAll(".navbar-nav .nav-item a").forEach(link => {
+        const linkPath = link.getAttribute("href").split("/").pop();
+        if (linkPath === currentPath) {
+            link.closest(".nav-item").classList.add("active");
+        }
+    });
 }
 
 
